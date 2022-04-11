e526f0b0bb7b43b15675417e6f1b6fbe377a9640
DeboraBSilva <deb.barussi@gmail.com>
2022-04-11 09:36:51 -0300

    Remove wallet_items relation with asset

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -3,6 +3,5 @@
 class Asset < ApplicationRecord
   validates :symbol, presence: true, uniqueness: true
 
-  has_many :wallet_items
   has_one :last_quote, -> { where(quotes: { current: true }) }, class_name: 'Quote'
 end
